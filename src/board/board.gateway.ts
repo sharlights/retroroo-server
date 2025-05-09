5a8c16fe5c934f7d414fd53b5e153e4c2db0f5e9
Scott Carey <scottazord@gmail.com>
2025-05-09 22:36:03 +0930

    WIP

--- src/board/board.gateway.ts
+++ src/board/board.gateway.ts
@@ -1,20 +1,7 @@
-import {
-  WebSocketGateway,
-  SubscribeMessage,
-  WebSocketServer,
-  ConnectedSocket,
-} from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-import { WsException } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class BoardGateway {
   @WebSocketServer() server: Server;
-
-  @SubscribeMessage('board:connect')
-  handleBoardConnect(@ConnectedSocket() socket: Socket) {
-    const user = socket.data.user;
-    if (!user) throw new WsException('Unauthorized');
-    return { boardId: user.boardId, userId: user.sub, role: user.role };
-  }
 }
