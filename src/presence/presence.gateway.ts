6a48bc48eee3ab26798a41ffdc9dbe6e909d5f43
Scott Carey <scottazord@gmail.com>
2025-05-10 12:33:15 +0930

    WIP

--- src/presence/presence.gateway.ts
+++ src/presence/presence.gateway.ts
@@ -3,15 +3,14 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketServer,
+  SubscribeMessage, ConnectedSocket,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { PresenceService } from './presence.service';
 import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway()
-export class PresenceGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
-{
+export class PresenceGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
@@ -24,22 +23,25 @@ export class PresenceGateway
     const token = client.handshake.auth?.token;
     const user = this.authService.validateToken(token);
 
-    const count = this.presenceService.addUser(
-      user.boardId,
-      user.sub,
-      client.id,
-    );
-    this.server
-      .to(user.boardId)
-      .emit('presence:total:update', { total: count });
+    client.data.user = user;
+    const count = this.presenceService.addUser(user.boardId, user.sub, client.id);
+    this.server.to(user.boardId).emit('presence:total:update', { total: count });
   }
 
   handleDisconnect(client: Socket) {
     const result = this.presenceService.removeUser(client.id);
     if (result) {
-      this.server
-        .to(result.boardId)
-        .emit('presence:total:update', { total: result.count });
+      this.server.to(result.boardId).emit('presence:total:update', { total: result.count });
     }
+  }
+
+  @SubscribeMessage('presence:join')
+  async handlePresenceJoin(@ConnectedSocket() socket: Socket) {
+    const user = socket.data.user;
+    const count = this.presenceService.addUser(user.boardId, user.sub, socket.id);
+    console.log(`[Board: ${user.boardId}] - presence:total: ${count}`);
+    this.server.to(user.boardId).emit('presence:total:update', { total: count });
+
+    return { total: count };
   }
 }
